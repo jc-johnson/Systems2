2fca3fd78e29a08dab1c4b6bfb55f987f21a1589
Jordan Johnson <jjohnson@myself.com>
2016-01-19 21:01:12 -0600

    homework 2 started with initial strcut declared

---
+++ hw2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int validBit;
+    int tag; // will have to bitwise total bits to get this value
+    int bBits;
+    int setIndex;
+} cache;
+
+typedef struct* set;
+typedef set* setPointer; // maybe call it memory later
+
+// TODO: add bitwise function to get tag bits and b bits.
+
+void main() {
+    
+    
+}
